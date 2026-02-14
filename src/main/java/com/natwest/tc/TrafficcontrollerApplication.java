30c95c1623c709567d347715e3d5bf9a014510e4
Mohan HK <mohahk@luxoft.com>
2026-02-14 19:22:50 +0530

    Initial project structure has been set up

---
+++ src/main/java/com/natwest/tc/TrafficcontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficcontrollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natwest/tc/TrafficcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natwest.tc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
